252b276b114dc3596648a4f384b1d35bb0ab51ce
Griffin Hackley <griffinhackley@yahoo.com>
2021-06-26 10:53:17 -0600

    Started data fusion. Had to change around the format of the JSONs a bit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 class projectOpen():
     def __init__(self, requirements, space):
@@ -22,18 +23,54 @@ class projectOpen():
             print("Assuming infinite space. Generating perfect configuration...")
         elif spExists and reqExists:
             print("Building schedule for the given space and requirements constraints....")
+            dataFusion(self.req, self.sp)
         else:
             print("Error 1")
             exit(1)
 
     def main(self):
-        print(self.req)
-        print(self.sp)
+        print()
+        # print(self.req)
+        # print(self.sp)
 
+def dataFusion(req, space):
+    #set up limits
+    limit = {}
+    for i in range (space['globals']['buildingClosedEnd'], 25):
+        limit[i] = {
+            'computers': space['computers'].copy(),
+            'spaces': space['spaces'].copy(),
+            'users': []
+        }
+        print(limit[i])
+    
+    #set initial matchups
+    for user in req['users']:
+        #start at their available start
+        start = user['availableStart']
+        end = user['availableEnd']
+        minSpace = user['space']
+        minComp = user['computer']
+        current = start
+
+        print(user['name'])
+
+        if limit[current]['spaces'][minSpace] > 0 and limit[current]['computers'][minComp] > 0:
+            limit[current]['spaces'][minSpace] = limit[current]['spaces'][minSpace] - 1
+            limit[current]['computers'][minComp] = limit[current]['computers'][minComp] -1
+            limit[current]['users'] = limit[current]['users'].append(user['name'])
+        
+    for i in range(space['globals']['buildingClosedEnd'], 25):
+        print(i, limit[i])
+        
 
 
 if __name__ == "__main__":
-    requirementsLocation = input(" Requirements location >>> ")
-    spaceLocation = input(" Space location >>> ")
+    # requirementsLocation = input(" Requirements location >>> ")
+    # spaceLocation = input(" Space location >>> ")
+    here = os.path.dirname(os.path.abspath(__file__))
+    requirementsLocation = open(os.path.join(here, "test1Requirements.json"),"r")
+    spaceLocation = open(os.path.join(here, "test1Space.json"),"r")
+
     pO = projectOpen(requirementsLocation, spaceLocation)
     pO.main()
